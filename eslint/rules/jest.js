6fc6b801214cc0570f677cd27392e5b80ff46536
Saswat Das <saswatd.official@gmail.com>
2024-04-23 00:21:03 +0530

    feat: update ESLint and add Jest configurations

--- eslint/rules/jest.js
+++ eslint/rules/jest.js
@@ -3,17 +3,17 @@ module.exports = {
     /**
      * Disallow duplicate setup and teardown hooks.
      * This rule prevents the use of duplicate setup and teardown hooks, which can lead to unexpected behavior and potential bugs.
-     * Reference: https://github.com/veritem/eslint-plugin-vitest/blob/HEAD/docs/rules/no-duplicate-hooks.md
+     * Reference: https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/no-duplicate-hooks.md
      * ❗️ This rule is not fixable.
      */
-    'vitest/no-duplicate-hooks': 'error',
+    'jest/no-duplicate-hooks': 'error',
 
     /**
      * Require lowercase test names.
      * This rule enforces the use of lowercase test names for better readability and consistency.
-     * Reference: https://github.com/veritem/eslint-plugin-vitest/blob/HEAD/docs/rules/prefer-lowercase-title.md
+     * Reference: https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/prefer-lowercase-title.md
      * ✅ This rule is fixable.
      */
-    'vitest/prefer-lowercase-title': 'warn',
+    'jest/prefer-lowercase-title': 'warn',
   },
 };
